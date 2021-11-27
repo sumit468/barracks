c1697c79c1171cb05f870e5dbe0f597176685fc6
Sumit <sumitskstool468@gmail.com>
2021-11-27 15:34:12 +0530

    App Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faCopyright } from '@fortawesome/free-regular-svg-icons';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccessoriesComponent } from './modules/accessories/accessories.component';
+import { FooterComponent } from './modules/footer/footer.component';
+import { HomeComponent } from './modules/home/home.component';
+import { LightsComponent } from './modules/lights/lights.component';
+import { MaintenanceComponent } from './modules/maintenance/maintenance.component';
+import { NavbarComponent } from './modules/navbar/navbar.component';
+import { ProtectionComponent } from './modules/protection/protection.component';
+import { ShopComponent } from './modules/shop/shop.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ShopComponent,
+    ProtectionComponent,
+    MaintenanceComponent,
+    LightsComponent,
+    AccessoriesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faFacebook, faInstagram, faShoppingCart, faCopyright);
+  }
+}
