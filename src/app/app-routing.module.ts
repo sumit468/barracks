0ccae6f10592ab2aa4f9a6ecb2aa2c3fa78a451b
Sumit <sumitskstool468@gmail.com>
2021-11-27 15:33:46 +0530

    Adding Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,58 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AccessoriesComponent } from './modules/accessories/accessories.component';
+import { HomeComponent } from './modules/home/home.component';
+import { LightsComponent } from './modules/lights/lights.component';
+import { MaintenanceComponent } from './modules/maintenance/maintenance.component';
+import { ProtectionComponent } from './modules/protection/protection.component';
+import { ShopComponent } from './modules/shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+    data: {
+      title: 'Home'
+    },
+  },
+  {
+    path: "shop",
+    component: ShopComponent,
+    data: {
+      title: 'Shop'
+    }
+  },
+  {
+    path: "protection",
+    component: ProtectionComponent,
+    data: {
+      title: 'Protection'
+    }
+  },
+  {
+    path: "maintenance",
+    component: MaintenanceComponent,
+    data: {
+      title: 'Maintenance'
+    }
+  },
+  {
+    path: "lights",
+    component: LightsComponent,
+    data: {
+      title: 'Lights'
+    }
+  },
+  {
+    path: "accessories",
+    component: AccessoriesComponent,
+    data: {
+      title: 'Lights'
+    }
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
