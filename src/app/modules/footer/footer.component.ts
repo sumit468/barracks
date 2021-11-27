49994efe576206763d9d0c1aed5404d799c9bde0
Sumit <sumitskstool468@gmail.com>
2021-11-27 15:36:38 +0530

    Added Components

---
+++ src/app/modules/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+  url = environment.url;
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: {
+    facebook: 'https://www.facebook.com/barracksguards',
+    instagram: 'https://www.instagram.com/bar_racks/?utm_medium=copy_link'
+  }
 };
 
 /*
